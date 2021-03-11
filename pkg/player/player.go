a3fdb1604f8648f173c6a20dc1513d1fc261013b
s.miyoshi <s.miyoshi1991@gmail.com>
2021-03-11 23:18:25 +0900

    add chip folder

--- pkg/player/player.go
+++ pkg/player/player.go
@@ -1,17 +1,23 @@
 package player
 
-// Player ...
-type Player struct {
-	HP        uint
-	HPMax     uint
-	ShotPower uint
-	Zenny     uint
-}
+import "github.com/sh-miyoshi/go-rockmanexe/pkg/chip"
 
 const (
 	defaultHP        uint = 100
 	defaultShotPower uint = 1
+
+	// FolderSize ...
+	FolderSize = 10 // debug
 )
+
+// Player ...
+type Player struct {
+	HP         uint
+	HPMax      uint
+	ShotPower  uint
+	Zenny      uint
+	ChipFolder [FolderSize]int
+}
 
 // New returns player data with default values
 func New() *Player {
@@ -20,6 +26,18 @@ func New() *Player {
 		HPMax:     defaultHP,
 		ShotPower: defaultShotPower,
 		Zenny:     0,
+		ChipFolder: [FolderSize]int{
+			chip.IDCannon,
+			chip.IDCannon,
+			chip.IDCannon,
+			chip.IDCannon,
+			chip.IDCannon,
+			chip.IDCannon,
+			chip.IDCannon,
+			chip.IDCannon,
+			chip.IDCannon,
+			chip.IDCannon,
+		},
 	}
 }
 
