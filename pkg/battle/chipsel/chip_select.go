a07a2e4ed20ca24ed553d2e3438f79870f1ae9cb
s.miyoshi <s.miyoshi1991@gmail.com>
2021-03-12 09:16:57 +0900

    show pointer

--- pkg/battle/chipsel/chip_select.go
+++ pkg/battle/chipsel/chip_select.go
@@ -1,17 +1,47 @@
 package chipsel
 
+import (
+	"fmt"
+
+	"github.com/sh-miyoshi/dxlib"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/common"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/player"
+)
+
 const (
 	selectMax = 5 // TODO should variable length
+	sendBtnNo = -1
 )
 
 var (
-	selectList []int
+	count = 0
+
+	selectList []player.ChipInfo
 	selected   []int
+
+	imgFrame   int32 = -1
+	imgPointer       = []int32{-1, -1}
+	pointer          = sendBtnNo
 )
 
 // Init ...
-func Init(folder []int) {
-	selectList = []int{}
+func Init(folder []player.ChipInfo) error {
+	if imgFrame == -1 {
+		fname := common.ImagePath + "battle/chip_select_frame.png"
+		imgFrame = dxlib.LoadGraph(fname)
+		if imgFrame == -1 {
+			return fmt.Errorf("Failed to read frame image: %s", fname)
+		}
+
+		fname = common.ImagePath + "battle/pointer.png"
+		res := dxlib.LoadDivGraph(fname, 2, 2, 1, 44, 44, imgPointer)
+		if res == -1 {
+			return fmt.Errorf("Failed to read pointer image: %s", fname)
+		}
+	}
+
+	count = 0
+	selectList = []player.ChipInfo{}
 	selected = []int{}
 
 	num := len(folder)
@@ -21,16 +51,46 @@ func Init(folder []int) {
 	for i := 0; i < num; i++ {
 		selectList = append(selectList, folder[i])
 	}
+
+	pointer = sendBtnNo
+	if num > 0 {
+		pointer = 0
+	}
+
+	return nil
 }
 
 // Draw ...
 func Draw() {
+	dxlib.DrawGraph(0, 0, imgFrame, dxlib.TRUE)
 
+	// TODO Show chip data
+	for i, s := range selectList {
+		// c := chip.Get(s.ID)
+		// Show Icon
+
+		// Show Detail Data
+		if i == pointer {
+			dxlib.DrawFormatString(30, 163, 0xffffff, "%s", s.Code)
+		}
+	}
+
+	// Show pointer
+	n := count / 20
+	if n%3 != 0 {
+		if pointer == sendBtnNo {
+			dxlib.DrawGraph(180, 225, imgPointer[1], dxlib.TRUE)
+		} else {
+			x := (pointer%5)*32 + 10
+			y := (pointer/5)*20 + 201
+			dxlib.DrawGraph(int32(x), int32(y), imgPointer[0], dxlib.TRUE)
+		}
+	}
 }
 
 // Process ...
 func Process() {
-
+	count++
 }
 
 // GetSelected ...
