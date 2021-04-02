b3741c8213157f2767c96ef454eaccfd76b5b7c8
s.miyoshi <s.miyoshi1991@gmail.com>
2021-04-02 15:00:25 +0900

    add init folder list

--- pkg/player/player.go
+++ pkg/player/player.go
@@ -56,6 +56,42 @@ func New() *Player {
 		WinNum:  0,
 		LoseNum: 0,
 	}
+
+	/* Correct Init Chip Folder
+	ChipFolder: [FolderSize]ChipInfo{
+		{ID: chip.IDCannon, Code: "b"},
+		{ID: chip.IDCannon, Code: "b"},
+		{ID: chip.IDCannon, Code: "c"},
+		{ID: chip.IDCannon, Code: "c"},
+		{ID: chip.IDHighCannon, Code: "d"},
+		{ID: chip.IDHighCannon, Code: "d"},
+		{ID: chip.IDMiniBomb, Code: "l"},
+		{ID: chip.IDMiniBomb, Code: "l"},
+		{ID: chip.IDMiniBomb, Code: "*"},
+		{ID: chip.IDMiniBomb, Code: "*"},
+		{ID: chip.IDSword, Code: "s"},
+		{ID: chip.IDSword, Code: "s"},
+		{ID: chip.IDSword, Code: "s"},
+		{ID: chip.IDSword, Code: "s"},
+		{ID: chip.IDWideSword, Code: "s"},
+		{ID: chip.IDWideSword, Code: "s"},
+		{ID: chip.IDRecover10, Code: "l"},
+		{ID: chip.IDRecover10, Code: "l"},
+		{ID: chip.IDRecover10, Code: "*"},
+		{ID: chip.IDRecover10, Code: "*"},
+
+		{ID: chip.IDRecover30, Code: "l"},
+		{ID: chip.IDRecover30, Code: "l"},
+		{ID: chip.IDバルカン1, Code: "b"},
+		{ID: chip.IDバルカン1, Code: "b"},
+		{ID: chip.IDバルカン1, Code: "d"},
+		{ID: chip.IDバルカン1, Code: "d"},
+		{ID: chip.IDスプレッドガン, Code: "n"},
+		{ID: chip.IDスプレッドガン, Code: "n"},
+		{ID: chip.IDスプレッドガン, Code: "m"},
+		{ID: chip.IDスプレッドガン, Code: "m"},
+	}
+	*/
 }
 
 // TODO NewWithSaveData(fname string) (*Player, error)
