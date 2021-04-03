ecad9ebbb229d9c65ad7e2c1bc51338440e94f0a
s.miyoshi <s.miyoshi1991@gmail.com>
2021-04-03 12:20:51 +0900

    add test

---
+++ pkg/chip/chip_test.go
@@ -0,0 +1,67 @@
+package chip
+
+import "testing"
+
+func TestSelectable(t *testing.T) {
+	tt := []struct {
+		CurrentList []SelectParam
+		Target      SelectParam
+		Expect      bool
+	}{
+		{ // Case 0(Same name)
+			CurrentList: []SelectParam{
+				{Name: "name", Code: "a"},
+				{Name: "name", Code: "b"},
+			},
+			Target: SelectParam{Name: "name", Code: "c"},
+			Expect: true,
+		},
+		{ // Case 1(Same code)
+			CurrentList: []SelectParam{
+				{Name: "name1", Code: "a"},
+				{Name: "name2", Code: "a"},
+			},
+			Target: SelectParam{Name: "name3", Code: "a"},
+			Expect: true,
+		},
+		{ // Case 2(Same code with * in list)
+			CurrentList: []SelectParam{
+				{Name: "name1", Code: "*"},
+				{Name: "name2", Code: "*"},
+			},
+			Target: SelectParam{Name: "name3", Code: "a"},
+			Expect: true,
+		},
+		{ // Case 3(Same code with * in target)
+			CurrentList: []SelectParam{
+				{Name: "name1", Code: "a"},
+				{Name: "name2", Code: "a"},
+			},
+			Target: SelectParam{Name: "name3", Code: "*"},
+			Expect: true,
+		},
+		{ // Case 4(Same code with * in both)
+			CurrentList: []SelectParam{
+				{Name: "name1", Code: "a"},
+				{Name: "name2", Code: "*"},
+			},
+			Target: SelectParam{Name: "name3", Code: "*"},
+			Expect: true,
+		},
+		{ // Case 5(No selectable)
+			CurrentList: []SelectParam{
+				{Name: "name1", Code: "a"},
+				{Name: "name2", Code: "a"},
+			},
+			Target: SelectParam{Name: "name1", Code: "b"},
+			Expect: false,
+		},
+	}
+
+	for i, tc := range tt {
+		res := Selectable(tc.Target, tc.CurrentList)
+		if res != tc.Expect {
+			t.Errorf("Selectable %d test failed. expect %v, but got %v", i, tc.Expect, res)
+		}
+	}
+}
