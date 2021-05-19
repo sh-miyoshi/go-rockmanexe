230590fae24f94427945c73d59a6445ca55f987d
s.miyoshi <s.miyoshi1991@gmail.com>
2021-05-19 11:35:40 +0900

    add client to server

--- cmd/testclient/main.go
+++ cmd/testclient/main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
+	"net/http"
 
+	routerapi "github.com/sh-miyoshi/go-rockmanexe/pkg/net/api/router"
 	pb "github.com/sh-miyoshi/go-rockmanexe/pkg/net/routerpb"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	// Add client to server
+	apiAddr := "http://localhost:8080"
+	httpRes, err := http.Post(apiAddr+"/api/v1/client", "text/plain", nil)
+	if err != nil {
+		log.Fatalf("Failed to add client: %v", err)
+	}
+	defer httpRes.Body.Close()
+	var res routerapi.ClientInfo
+	if httpRes.StatusCode == http.StatusOK {
+		if err := json.NewDecoder(httpRes.Body).Decode(&res); err != nil {
+			log.Fatalf("Failed to decode client add response: %v", err)
+		}
+		log.Printf("Client Info: %+v", res)
+	} else {
+		log.Fatalf("Client add request returns unexpected status %s", httpRes.Status)
+	}
+
 	serverAddr := "localhost:80"
 	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
 	if err != nil {
@@ -18,8 +38,8 @@ func main() {
 
 	client := pb.NewRouterClient(conn)
 	req := &pb.AuthRequest{
-		Id:      "tester",
-		Key:     "testclient",
+		Id:      res.ID,
+		Key:     res.Key,
 		Version: "test",
 	}
 	dataStream, err := client.PublishData(context.TODO(), req)
