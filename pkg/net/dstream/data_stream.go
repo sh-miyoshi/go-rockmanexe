234d9b039104ec7826243593953f694b8a8520c4
s.miyoshi <s.miyoshi1991@gmail.com>
2021-05-27 14:19:40 +0900

    refactoring

--- pkg/net/dstream/data_stream.go
+++ pkg/net/dstream/data_stream.go
@@ -2,7 +2,6 @@ package dstream
 
 import (
 	"context"
-	"errors"
 	"fmt"
 
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/logger"
@@ -23,7 +22,10 @@ func New() *RouterStream {
 
 func (s *RouterStream) SendAction(ctx context.Context, action *pb.Action) (*pb.Result, error) {
 	if err := session.ActionProc(action); err != nil {
-		return nil, fmt.Errorf("request failed: %w", err)
+		return &pb.Result{
+			Success: false,
+			ErrMsg:  fmt.Sprintf("request failed: %v", err),
+		}, nil
 	}
 
 	return &pb.Result{
@@ -33,33 +35,36 @@ func (s *RouterStream) SendAction(ctx context.Context, action *pb.Action) (*pb.R
 
 func (s *RouterStream) PublishData(authReq *pb.AuthRequest, dataStream pb.Router_PublishDataServer) error {
 	// Verify auth request
+	authRes := &pb.AuthResponse{}
+
 	// TODO validate version
 	c, err := db.GetInst().ClientGetByID(authReq.Id)
 	if err != nil {
 		logger.Info("Failed to get client: %v", err)
-		return errors.New("authenticate failed")
+		authRes.Success = false
+		authRes.ErrMsg = "authenticate failed"
+		dataStream.Send(makeAuthRes(authRes))
+		return nil
 	}
 	if c.Key != authReq.Key {
 		logger.Info("got invalid key from user")
-		return errors.New("authenticate failed")
+		authRes.Success = false
+		authRes.ErrMsg = "authenticate failed"
+		dataStream.Send(makeAuthRes(authRes))
+		return nil
 	}
 
 	// Add to sessionList
 	sid, err := session.Add(c.ID, s.sendQueue)
 	if err != nil {
 		logger.Error("Failed to add session: %v", err)
+		return fmt.Errorf("add session failed: %w", err)
 	}
 	logger.Info("add to session %s", sid)
 
-	dataStream.Send(&pb.Data{
-		Type: pb.Data_AUTHRESPONSE,
-		Data: &pb.Data_AuthRes{
-			AuthRes: &pb.AuthResponse{
-				Success:   true,
-				SessionID: sid,
-			},
-		},
-	})
+	authRes.Success = true
+	authRes.SessionID = sid
+	dataStream.Send(makeAuthRes(authRes))
 
 	session.Run(sid)
 
@@ -69,5 +74,14 @@ func (s *RouterStream) PublishData(authReq *pb.AuthRequest, dataStream pb.Router
 		logger.Debug("Send to client %s: %+v", c.ID, data)
 
 		dataStream.Send(data)
+	}
+}
+
+func makeAuthRes(authRes *pb.AuthResponse) *pb.Data {
+	return &pb.Data{
+		Type: pb.Data_AUTHRESPONSE,
+		Data: &pb.Data_AuthRes{
+			AuthRes: authRes,
+		},
 	}
 }
