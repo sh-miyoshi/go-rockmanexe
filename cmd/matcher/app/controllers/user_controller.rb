0c17d07602791f5b00155975d6ea9048893a3d3f
s.miyoshi <s.miyoshi1991@gmail.com>
2021-08-24 09:16:47 +0900

    show user info

--- cmd/matcher/app/controllers/user_controller.rb
+++ cmd/matcher/app/controllers/user_controller.rb
@@ -1,4 +1,6 @@
 class UserController < ApplicationController
+  before_action :set_login_user, except: :index
+
   def index
   end
 
@@ -12,5 +14,12 @@ class UserController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_login_user
+    login_id = "login_id_1" # debug
+    @user = User.find_by(login_id: login_id)
   end
 end
