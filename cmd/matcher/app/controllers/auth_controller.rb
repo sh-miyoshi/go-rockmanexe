4e359fd709c48bcc308badd586a74f7f6edd8d32
s.miyoshi <s.miyoshi1991@gmail.com>
2021-09-04 22:05:58 +0900

    add signup link

--- cmd/matcher/app/controllers/auth_controller.rb
+++ cmd/matcher/app/controllers/auth_controller.rb
@@ -1,8 +1,8 @@
 class AuthController < ApplicationController
   def callback
     auth_info = request.env['omniauth.auth']
-    session[:userinfo] = auth_info['extra']['raw_info']
     Rails.logger.debug("login info: #{auth_info}")
+    session[:user_id] = auth_info[:uid]
 
     redirect_to '/user/show'
   end
