b5b165301fa6797ae6825ea26d9f14b0d4969e63
s.miyoshi <s.miyoshi1991@gmail.com>
2021-09-05 11:04:40 +0900

    fix rubocop error

--- cmd/matcher/app/controllers/auth_controller.rb
+++ cmd/matcher/app/controllers/auth_controller.rb
@@ -17,18 +17,19 @@ class AuthController < ApplicationController
   end
 
   private
+
   AUTH0_CONFIG = Rails.application.config_for(:auth0)
 
   def logout_url
     request_params = {
       returnTo: root_url,
-      client_id: AUTH0_CONFIG['auth0_client_id']
+      client_id: AUTH0_CONFIG["auth0_client_id"]
     }
 
-    URI::HTTPS.build(host: AUTH0_CONFIG['auth0_domain'], path: '/v2/logout', query: to_query(request_params)).to_s
+    URI::HTTPS.build(host: AUTH0_CONFIG["auth0_domain"], path: "/v2/logout", query: to_query(request_params)).to_s
   end
 
   def to_query(hash)
-    hash.map { |k, v| "#{k}=#{CGI.escape(v)}" unless v.nil? }.reject(&:nil?).join('&')
+    hash.map { |k, v| "#{k}=#{CGI.escape(v)}" unless v.nil? }.reject(&:nil?).join("&")
   end
 end
