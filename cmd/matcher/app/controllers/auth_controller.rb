a44d350714bcf66022047d5a66ca67f434a7a2a0
s.miyoshi <s.miyoshi1991@gmail.com>
2021-09-05 10:57:04 +0900

    implement logout

--- cmd/matcher/app/controllers/auth_controller.rb
+++ cmd/matcher/app/controllers/auth_controller.rb
@@ -12,6 +12,23 @@ class AuthController < ApplicationController
   end
 
   def logout
-    # TODO
+    reset_session
+    redirect_to logout_url
+  end
+
+  private
+  AUTH0_CONFIG = Rails.application.config_for(:auth0)
+
+  def logout_url
+    request_params = {
+      returnTo: root_url,
+      client_id: AUTH0_CONFIG['auth0_client_id']
+    }
+
+    URI::HTTPS.build(host: AUTH0_CONFIG['auth0_domain'], path: '/v2/logout', query: to_query(request_params)).to_s
+  end
+
+  def to_query(hash)
+    hash.map { |k, v| "#{k}=#{CGI.escape(v)}" unless v.nil? }.reject(&:nil?).join('&')
   end
 end
