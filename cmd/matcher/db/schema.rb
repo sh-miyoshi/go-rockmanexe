f4f925cac532504fa7bc382339503e811fc674ae
s.miyoshi <s.miyoshi1991@gmail.com>
2021-09-06 19:09:41 +0900

    add user id

--- cmd/matcher/db/schema.rb
+++ cmd/matcher/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_091428) do
+ActiveRecord::Schema.define(version: 2021_09_05_124113) do
 
   create_table "histories", force: :cascade do |t|
     t.text "users"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_09_05_091428) do
     t.string "login_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "user_id", null: false
   end
 
 end
--- cmd/matcher/db/seeds.rb
+++ cmd/matcher/db/seeds.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-User.create(name: "tester1", login_id: "login_id_1")
+User.create(name: "tester1", login_id: "login_id_1", user_id: SecureRandom.uuid)
