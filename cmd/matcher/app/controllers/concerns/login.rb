69e981354f4cc90c314b06fcc8b9944d828d55b7
s.miyoshi <s.miyoshi1991@gmail.com>
2021-09-12 21:27:23 +0900

    check logged in

--- cmd/matcher/app/controllers/concerns/login.rb
+++ cmd/matcher/app/controllers/concerns/login.rb
@@ -5,11 +5,13 @@ module Login
 
   def login_user
     # redirect top page if not logged in
-    # return redirect_to '/' unless session[:user_id].present?
+    return redirect_to "/" unless session[:user_id].present?
 
-    # Userがいなければ作成ページにリダイレクト
+    user = User.find_by(login_id: session[:user_id])
 
-    # debug
-    User.find_by(login_id: "login_id_1")
+    # redirect user create page if not in matching server
+    return redirect_to user_new_path if user.nil?
+
+    user
   end
 end
