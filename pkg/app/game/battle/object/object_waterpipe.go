826946e6d4875794b00c78763bb4693c277a6cc0
s.miyoshi <s.miyoshi1991@gmail.com>
2021-11-21 22:47:56 +0900

    show water pipe attack

--- pkg/app/game/battle/object/object_waterpipe.go
+++ pkg/app/game/battle/object/object_waterpipe.go
@@ -13,19 +13,19 @@ import (
 )
 
 const (
-	delayWaterPipeSet = 3
+	delayWaterPipeSet    = 3
+	delayWaterPipeAttack = 8
 )
 
-// type WaterPipeAtk struct {
-// 	id      string
-// 	ownerID string
-// 	count   int
-// 	images  []int32
-// }
+type WaterPipeAtk struct {
+	count       int
+	images      []int32
+	isAttacking bool
+}
 
 type WaterPipe struct {
-	pm ObjectParam
-	// atk    WaterPipeAtk
+	pm     ObjectParam
+	atk    WaterPipeAtk
 	imgSet []int32
 	count  int
 }
@@ -33,7 +33,6 @@ type WaterPipe struct {
 func (o *WaterPipe) Init(ownerID string, initParam ObjectParam) error {
 	o.pm = initParam
 	o.pm.ObjectID = uuid.New().String()
-	// o.atk.ownerID = ownerID
 
 	// Load Images
 	o.imgSet = make([]int32, 4)
@@ -41,6 +40,11 @@ func (o *WaterPipe) Init(ownerID string, initParam ObjectParam) error {
 	if res := dxlib.LoadDivGraph(fname, 4, 4, 1, 72, 88, o.imgSet); res == -1 {
 		return fmt.Errorf("failed to load image: %s", fname)
 	}
+
+	if err := o.atk.Init(); err != nil {
+		return fmt.Errorf("failed to init water pipe attack %w", err)
+	}
+
 	return nil
 }
 
@@ -49,17 +53,33 @@ func (o *WaterPipe) End() {
 	for _, img := range o.imgSet {
 		dxlib.DeleteGraph(img)
 	}
+
+	o.atk.End()
 }
 
 func (o *WaterPipe) Process() (bool, error) {
+	if o.atk.IsAttacking() {
+		o.atk.Process()
+		return false, nil
+	}
+
 	o.count++
+
+	// TODO
+	if o.count == 50 {
+		o.atk.Start()
+	}
+
 	return false, nil
 }
 
 func (o *WaterPipe) Draw() {
 	x, y := battlecommon.ViewPos(o.pm.PosX, o.pm.PosY)
 
-	// TODO: 攻撃時
+	if o.atk.IsAttacking() {
+		o.atk.Draw(x, y)
+		return
+	}
 
 	n := o.count / delayWaterPipeSet
 	if n > len(o.imgSet)-1 {
@@ -84,4 +104,51 @@ func (o *WaterPipe) GetParam() anim.Param {
 
 func (o *WaterPipe) GetObjectType() int {
 	return objanim.ObjTypeNone
+}
+
+func (a *WaterPipeAtk) Init() error {
+	a.count = 0
+	a.isAttacking = false
+	a.images = make([]int32, 9)
+	fname := common.ImagePath + "battle/character/水道管_atk.png"
+	if res := dxlib.LoadDivGraph(fname, 9, 9, 1, 234, 110, a.images); res == -1 {
+		return fmt.Errorf("failed to load image: %s", fname)
+	}
+	return nil
+}
+
+func (a *WaterPipeAtk) End() {
+	for _, img := range a.images {
+		dxlib.DeleteGraph(img)
+	}
+}
+
+func (a *WaterPipeAtk) Start() {
+	a.isAttacking = true
+}
+
+func (a *WaterPipeAtk) IsAttacking() bool {
+	return a.isAttacking
+}
+
+func (a *WaterPipeAtk) Draw(x, y int32) {
+	n := 0
+	if a.isAttacking {
+		c := (a.count / delayWaterPipeAttack) % (len(a.images) * 2)
+		s := len(a.images)
+		n = c - (c/s)*((c-s)*2+1)
+	}
+
+	dxlib.DrawRotaGraph(x-81, y+13, 1, 0, a.images[n], dxlib.TRUE)
+}
+
+func (a *WaterPipeAtk) Process() {
+	a.count++
+
+	// TODO(ダメージ)
+
+	if a.count > len(a.images)*2*delayWaterPipeAttack {
+		a.count = 0
+		a.isAttacking = false
+	}
 }
