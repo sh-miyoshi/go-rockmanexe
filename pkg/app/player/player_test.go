9e8822b53a9feb29bf8f011e7f6632469c39c272
s.miyoshi <s.miyoshi1991@gmail.com>
2021-12-26 18:11:50 +0900

    fix test

--- pkg/app/player/player_test.go
+++ pkg/app/player/player_test.go
@@ -1,36 +1,75 @@
 package player
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"os"
 	"testing"
 )
 
 func TestSave(t *testing.T) {
-	player := Player{
-		HP:         1,
-		ShotPower:  2,
-		Zenny:      4,
-		ChipFolder: [FolderSize]ChipInfo{},
-
-		WinNum: 3,
-	}
-	expect := "development#0#1#2#4#3#"
-	for i := 0; i < FolderSize; i++ {
-		expect += "0#"
+	plyr := Player{
+		HP:        100,
+		ShotPower: 5,
 	}
 
 	tmpfile, _ := ioutil.TempFile("", "")
 	defer os.Remove(tmpfile.Name())
 
 	// Save with no encryption
-	if err := player.Save(tmpfile.Name(), nil); err != nil {
+	if err := plyr.Save(tmpfile.Name(), nil); err != nil {
 		t.Errorf("Failed to save data: %+v", err)
 	}
 
 	tmpfile.Close()
-	res, _ := ioutil.ReadFile(tmpfile.Name())
-	if string(res) != expect {
-		t.Errorf("Player info save failed. expect %s, but got %s", expect, string(res))
+
+	var res SaveData
+	rawRes, _ := ioutil.ReadFile(tmpfile.Name())
+	json.Unmarshal(rawRes, &res)
+	if res.Player.HP != plyr.HP || res.Player.ShotPower != plyr.ShotPower {
+		t.Errorf("Player info save failed. expect %+v, but got %+v", plyr, res)
+	}
+}
+
+func TestNewWithSaveData(t *testing.T) {
+	tt := []struct {
+		caseName string
+		input    string
+		expectOK bool
+	}{
+		{
+			"v0.3 save data",
+			`{"player":{"hp":200,"shot_power":1,"zenny":0,"chip_folder":[{"id":1,"code":"b"},{"id":1,"code":"b"},{"id":1,"code":"c"},{"id":1,"code":"c"},{"id":2,"code":"d"},{"id":2,"code":"d"},{"id":44,"code":"l"},{"id":44,"code":"l"},{"id":44,"code":"*"},{"id":44,"code":"*"},{"id":54,"code":"s"},{"id":54,"code":"s"},{"id":54,"code":"s"},{"id":54,"code":"s"},{"id":55,"code":"s"},{"id":55,"code":"s"},{"id":109,"code":"l"},{"id":109,"code":"l"},{"id":109,"code":"*"},{"id":109,"code":"*"},{"id":110,"code":"l"},{"id":110,"code":"l"},{"id":5,"code":"b"},{"id":5,"code":"b"},{"id":5,"code":"d"},{"id":5,"code":"d"},{"id":8,"code":"n"},{"id":8,"code":"n"},{"id":8,"code":"m"},{"id":8,"code":"m"}],"win_num":0,"play_count":2101,"back_pack":[],"battle_histories":[{"opponent_id":"804f3fc5-328f-49c6-969d-ac559d95108a","date":"2021-10-16T19:29:09.1331744+09:00","is_win":true}]},"program_version":"v0.3"}`,
+			true,
+		},
+		{
+			"v0.4 save data",
+			`{"player":{"hp":200,"shot_power":1,"zenny":0,"chip_folder":[{"id":1,"code":"b"},{"id":1,"code":"b"},{"id":1,"code":"c"},{"id":1,"code":"c"},{"id":2,"code":"d"},{"id":2,"code":"d"},{"id":44,"code":"l"},{"id":44,"code":"l"},{"id":44,"code":"*"},{"id":44,"code":"*"},{"id":54,"code":"s"},{"id":54,"code":"s"},{"id":54,"code":"s"},{"id":54,"code":"s"},{"id":55,"code":"s"},{"id":55,"code":"s"},{"id":109,"code":"l"},{"id":109,"code":"l"},{"id":109,"code":"*"},{"id":109,"code":"*"},{"id":110,"code":"l"},{"id":110,"code":"l"},{"id":5,"code":"b"},{"id":5,"code":"b"},{"id":5,"code":"d"},{"id":5,"code":"d"},{"id":8,"code":"n"},{"id":8,"code":"n"},{"id":8,"code":"m"},{"id":8,"code":"m"}],"win_num":0,"play_count":2101,"back_pack":[{"id":8,"code":"m"},{"id":8,"code":"m"}],"battle_histories":[{"opponent_id":"804f3fc5-328f-49c6-969d-ac559d95108a","date":"2021-10-16T19:29:09.1331744+09:00","is_win":true}]},"program_version":"v0.4"}`,
+			true,
+		},
+		{
+			"development save data",
+			`{"player":{},"program_version":"development"}`,
+			true,
+		},
+		{
+			"invalid save data",
+			`{"invalid": "test message"}`,
+			false,
+		},
+	}
+
+	tmpfile, _ := ioutil.TempFile("", "")
+	defer os.Remove(tmpfile.Name())
+
+	for _, tc := range tt {
+		ioutil.WriteFile(tmpfile.Name(), []byte(tc.input), 0644)
+		_, err := NewWithSaveData(tmpfile.Name(), nil)
+		if tc.expectOK && err != nil {
+			t.Errorf("Test case %s expects ok, but got %v", tc.caseName, err)
+		}
+		if !tc.expectOK && err == nil {
+			t.Errorf("Test case %s expects error, but got nil", tc.caseName)
+		}
 	}
 }
