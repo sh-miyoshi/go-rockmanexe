c6475d7bc141d9e0f2038015de619d88da7592aa
s.miyoshi <s.miyoshi1991@gmail.com>
2022-01-17 21:43:59 +0900

    refactoring

---
+++ pkg/app/game/battle/opening/opening_normal.go
@@ -0,0 +1,84 @@
+package opening
+
+import (
+	"github.com/sh-miyoshi/dxlib"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/config"
+	battlecommon "github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/common"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/enemy"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/sound"
+)
+
+const (
+	normalViewDelay = 8
+	normalViewCount = 256 / normalViewDelay
+)
+
+type normal struct {
+	count     int
+	showCount int
+	enemies   []enemy.EnemyParam
+	images    map[int]int32
+}
+
+func (n *normal) Init(enemyList []enemy.EnemyParam) error {
+	n.count = 0
+	n.showCount = 0
+	n.enemies = enemyList
+	n.images = make(map[int]int32)
+
+	for _, e := range enemyList {
+		_, ok := n.images[e.CharID]
+		if !ok {
+			name, ext := enemy.GetStandImageFile(e.CharID)
+			fname := name + ext
+			n.images[e.CharID] = dxlib.LoadGraph(fname)
+		}
+	}
+
+	return nil
+}
+func (n *normal) End() {
+	for _, img := range n.images {
+		dxlib.DeleteGraph(img)
+	}
+	n.images = make(map[int]int32)
+}
+func (n *normal) Process() bool {
+	if config.Get().Debug.SkipBattleOpening {
+		return true
+	}
+
+	if n.count == 0 {
+		sound.On(sound.SEEnemyAppear)
+	}
+
+	n.count++
+	if n.count > normalViewCount {
+		n.count = 0
+		n.showCount++
+		if n.showCount >= len(n.enemies) {
+			return true
+		}
+	}
+	return false
+}
+func (n *normal) Draw() {
+	// Show animationed enemies
+	for i := 0; i < n.showCount; i++ {
+		x, y := battlecommon.ViewPos(n.enemies[i].PosX, n.enemies[i].PosY)
+		dxlib.DrawRotaGraph(x, y, 1, 0, n.images[n.enemies[i].CharID], dxlib.TRUE)
+	}
+
+	// Show current enemy
+	if n.showCount < len(n.enemies) {
+		pm := int32(n.count * normalViewDelay)
+		if pm >= 256 {
+			pm = 255
+		}
+
+		dxlib.SetDrawBlendMode(dxlib.DX_BLENDMODE_ALPHA, pm)
+		x, y := battlecommon.ViewPos(n.enemies[n.showCount].PosX, n.enemies[n.showCount].PosY)
+		dxlib.DrawRotaGraph(x, y, 1, 0, n.images[n.enemies[n.showCount].CharID], dxlib.TRUE)
+		dxlib.SetDrawBlendMode(dxlib.DX_BLENDMODE_NOBLEND, 0)
+	}
+}
