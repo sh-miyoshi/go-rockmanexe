649efc6a0bdf82adc928116df5c4296753039520
s.miyoshi <s.miyoshi1991@gmail.com>
2023-04-08 10:22:46 +0900

    support high/mega cannon

--- pkg/router/skill/skill.go
+++ pkg/router/skill/skill.go
@@ -1,6 +1,8 @@
 package skill
 
 import (
+	"fmt"
+
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/chip"
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/anim"
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/router/gameinfo"
@@ -26,6 +28,11 @@ func GetByChip(chipID int, arg Argument) SkillAnim {
 	switch chipID {
 	case chip.IDCannon:
 		return newCannon(TypeNormalCannon, arg)
+	case chip.IDHighCannon:
+		return newCannon(TypeHighCannon, arg)
+	case chip.IDMegaCannon:
+		return newCannon(TypeMegaCannon, arg)
+	default:
+		panic(fmt.Sprintf("chip %d is not implemented yet", chipID))
 	}
-	return nil
 }
