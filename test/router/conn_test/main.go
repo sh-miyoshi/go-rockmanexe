ce5eee8fab64d0d2e39d143a0572607250d36b71
s.miyoshi <s.miyoshi1991@gmail.com>
2023-04-22 13:35:15 +0900

    add app process

--- test/router/conn_test/main.go
+++ test/router/conn_test/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"errors"
+	"math/rand"
 	"os"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	netconn "github.com/sh-miyoshi/go-rockmanexe/pkg/app/netconn"
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/logger"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/net/action"
+	pb "github.com/sh-miyoshi/go-rockmanexe/pkg/net/netconnpb"
+	netobj "github.com/sh-miyoshi/go-rockmanexe/pkg/net/object"
+)
+
+const (
+	stateWaiting int = iota
+	stateOpening
+	stateChipSelect
+	stateWaitSelect
+	stateBeforeMain
+	stateMain
+	stateResult
 )
 
 const (
@@ -23,13 +38,17 @@ const (
 func main() {
 	logger.InitLogger(true, "")
 
+	// 1. 同時通信
 	var wg sync.WaitGroup
-	wg.Add(2)
-	go runClient("tester1", &wg)
-	go runClient("tester2", &wg)
+	wg.Add(4)
+	go runClient("tester1", &wg, true)
+	go runClient("tester2", &wg, false)
+
+	go runClient("tester3", &wg, true)
+	go runClient("tester4", &wg, false)
 
 	wg.Wait()
-	logger.Info("Finished first battle for tester1, tester2")
+	logger.Info("Successfully finished multiple sessions")
 
 	// TODO
 
@@ -45,8 +64,11 @@ func exitByError(err error) {
 	os.Exit(1)
 }
 
-func runClient(clientID string, wg *sync.WaitGroup) {
+func runClient(clientID string, wg *sync.WaitGroup, isWinner bool) {
 	defer wg.Done()
+
+	n := rand.Intn(30)
+	time.Sleep(time.Duration(n) * time.Millisecond)
 
 	conn := netconn.New(netconn.Config{
 		StreamAddr:     streamAddr,
@@ -74,5 +96,58 @@ func runClient(clientID string, wg *sync.WaitGroup) {
 	}
 	logger.Info("Successfully connected client %s to router", clientID)
 
-	// TODO
+	startTime := time.Now()
+	appStatus := stateWaiting
+	atkFlag := false
+	for {
+		current := time.Now()
+		const timeout = 5 * time.Second
+		if current.After(startTime.Add(timeout)) {
+			exitByError(errors.New("timeout main loop"))
+		}
+
+		switch appStatus {
+		case stateWaiting:
+			status := conn.GetGameStatus()
+			if status == pb.Response_CHIPSELECTWAIT {
+				obj := netobj.InitParam{
+					ID: uuid.New().String(),
+					HP: 10,
+					X:  1,
+					Y:  1,
+				}
+				conn.SendSignal(pb.Request_INITPARAMS, obj.Marshal())
+				stateChange(&appStatus, stateChipSelect)
+			}
+		case stateChipSelect:
+			conn.SendSignal(pb.Request_CHIPSELECT, nil)
+			stateChange(&appStatus, stateWaitSelect)
+		case stateWaitSelect:
+			status := conn.GetGameStatus()
+			if status == pb.Response_ACTING {
+				stateChange(&appStatus, stateMain)
+				continue
+			}
+		case stateMain:
+			if isWinner && !atkFlag {
+				atkFlag = true
+				buster := action.Buster{
+					Power: 10,
+				}
+				conn.SendAction(pb.Request_BUSTER, buster.Marshal())
+			}
+
+			status := conn.GetGameStatus()
+			if status == pb.Response_GAMEEND {
+				logger.Info("got game end at %s", clientID)
+				return
+			}
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+func stateChange(state *int, next int) {
+	logger.Info("state change from %d to %d", *state, next)
+	*state = next
 }
