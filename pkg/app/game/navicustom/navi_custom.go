a7ff5bf9a88eb58c73ff57ce976fdb4e447fb59e
s.miyoshi <s.miyoshi1991@gmail.com>
2023-08-20 10:19:23 +0900

    add state change

--- pkg/app/game/navicustom/navi_custom.go
+++ pkg/app/game/navicustom/navi_custom.go
@@ -1,11 +1,55 @@
 package navicustom
 
+import (
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/inputs"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/logger"
+)
+
+const (
+	stateOpening int = iota
+	stateMain
+	stateRun
+)
+
+var (
+	state int
+	count int
+)
+
 func Init() error {
+	state = stateOpening
+	count = 0
 	return nil
 }
 
 func Draw() {
+	switch state {
+	case stateOpening:
+		// 起動時アニメーション
+	case stateMain:
+		// 実際にパーツを置いたりする
+	case stateRun:
+		// RUN
+	}
 }
 
 func Process() {
+	switch state {
+	case stateOpening:
+		if count > 30 || (count > 3 && inputs.CheckKey(inputs.KeyEnter) > 0) {
+			stateChange(stateMain)
+		}
+	case stateMain:
+		// TODO
+	case stateRun:
+		// TODO
+	}
+
+	count++
+}
+
+func stateChange(next int) {
+	logger.Info("Change navu cutom state from %d to %d", state, next)
+	state = next
+	count = 0
 }
