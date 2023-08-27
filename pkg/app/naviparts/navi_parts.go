3a65a223b24ca90591c32cd24c7ca6a7fbea1e86
s.miyoshi <s.miyoshi1991@gmail.com>
2023-08-27 16:22:06 +0900

    add navi custom data

--- pkg/app/naviparts/navi_parts.go
+++ pkg/app/naviparts/navi_parts.go
@@ -1,20 +1,61 @@
 package naviparts
 
-/*
-// 実装予定のパーツ
-アタック+1
-ラピッド+1
-チャージ+1
-HP+50・HP+100
-カスタム1
-フロートシューズ・エアシューズ
-アンダーシャツ
-クイックゲージ
-*/
+import (
+	"fmt"
+
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/common"
+)
+
+// Memo: 色情報は固定で持たせる。同じ種類で色が異なる場合は別IDを割り当てる
+
+const (
+	ColorWhite int = iota
+	ColorYellow
+	ColorPink
+)
+
+const (
+	IDAttack1 int = iota
+	IDRapid1
+	IDCharge1
+	IDHP50
+	IDHP100
+	IDCustom1
+	IDUnderShirt
+	IDQuickGauge
+)
 
 type NaviParts struct {
-	// TODO: 形
+	ID          int
+	Name        string
+	Blocks      []common.Point
 	IsPlusParts bool
 	Color       int
 	// TODO: 効果
+}
+
+var (
+	allParts = []NaviParts{
+		{
+			ID:   IDAttack1,
+			Name: "アタック+1",
+			Blocks: []common.Point{
+				{X: 0, Y: 0},
+				{X: 0, Y: 1},
+			},
+			IsPlusParts: true,
+			Color:       ColorPink,
+		},
+	}
+)
+
+func Get(id int) NaviParts {
+	for _, parts := range allParts {
+		if parts.ID == id {
+			return parts
+		}
+	}
+
+	common.SetError(fmt.Sprintf("Navi parts %d is not implemented yet", id))
+	return NaviParts{}
 }
