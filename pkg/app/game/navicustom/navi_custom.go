4a83cea6a59737758b25f5bd3a2d6bdcfcb7e4c9
s.miyoshi <s.miyoshi1991@gmail.com>
2023-09-02 11:45:49 +0900

    show parts list

--- pkg/app/game/navicustom/navi_custom.go
+++ pkg/app/game/navicustom/navi_custom.go
@@ -8,8 +8,16 @@ import (
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/fade"
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/naviparts"
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/player"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/resources"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/sound"
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/dxlib"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/inputs"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/list"
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/logger"
+)
+
+const (
+	maxListNum = 5
 )
 
 const (
@@ -19,25 +27,34 @@ const (
 )
 
 var (
-	state    int
-	count    int
-	imgBack  int = -1
-	imgBoard int = -1
+	state          int
+	count          int
+	imgBack        int = -1
+	imgBoard       int = -1
+	imgListPointer int = -1
 	// playerInfo *player.Player // TODO: 更新のタイミングで使う
 	unsetParts []player.NaviCustomParts
+	itemList   list.ItemList
+	selected   int
 )
 
 func Init(plyr *player.Player) error {
 	// playerInfo = plyr
 	unsetParts = []player.NaviCustomParts{}
+	names := []string{}
 	for _, p := range plyr.AllNaviCustomParts {
 		if !p.IsSet {
 			unsetParts = append(unsetParts, p)
+			parts := naviparts.Get(p.ID)
+			names = append(names, parts.Name)
 		}
 	}
+	itemList.SetList(names, maxListNum)
 
 	state = stateOpening
 	count = 0
+	selected = -1
+
 	fname := common.ImagePath + "naviCustom/back.png"
 	imgBack = dxlib.LoadGraph(fname)
 	if imgBack == -1 {
@@ -48,12 +65,19 @@ func Init(plyr *player.Player) error {
 	if imgBoard == -1 {
 		return fmt.Errorf("failed to load board image")
 	}
+	fname = common.ImagePath + "menu/arrow.png"
+	imgListPointer = dxlib.LoadGraph(fname)
+	if imgListPointer == -1 {
+		return fmt.Errorf("failed to load arrow image")
+	}
+
 	return nil
 }
 
 func End() {
 	dxlib.DeleteGraph(imgBack)
 	dxlib.DeleteGraph(imgBoard)
+	dxlib.DeleteGraph(imgListPointer)
 }
 
 func Draw() {
@@ -65,11 +89,14 @@ func Draw() {
 		// Nothing to do
 	case stateMain:
 		// 実際にパーツを置いたりする
-		for i, p := range unsetParts {
-			parts := naviparts.Get(p.ID)
-			dxlib.DrawBox(300, i*30+40, 400, i*30+65, dxlib.GetColor(16, 80, 104), true)
-			draw.String(305, i*30+42, 0xFFFFFF, "%s", parts.Name)
+		for i, name := range itemList.GetList() {
+			drawPartsListItem(300, i*30+45, name)
 		}
+		if selected == -1 || (count/3)%2 == 0 {
+			dxlib.DrawGraph(280, itemList.GetPointer()*30+50, imgListPointer, true)
+		}
+
+		// TODO: ミニウィンドウ
 	case stateRun:
 		// RUN
 	}
@@ -87,6 +114,17 @@ func Process() {
 		}
 	case stateMain:
 		// TODO
+		if selected == -1 {
+			selected = itemList.Process()
+			if selected != -1 {
+				sound.On(resources.SEMenuEnter)
+			}
+		} else {
+			if inputs.CheckKey(inputs.KeyCancel) == 1 {
+				selected = -1
+				return
+			}
+		}
 	case stateRun:
 		// TODO
 	}
@@ -98,4 +136,10 @@ func stateChange(next int) {
 	logger.Info("Change navu cutom state from %d to %d", state, next)
 	state = next
 	count = 0
+}
+
+func drawPartsListItem(x, y int, name string) {
+	dxlib.DrawBox(x-2, y-1, x+102, y+26, dxlib.GetColor(168, 192, 216), true)
+	dxlib.DrawBox(x, y, x+100, y+25, dxlib.GetColor(16, 80, 104), true)
+	draw.String(x+5, y+2, 0xFFFFFF, "%s", name)
 }
