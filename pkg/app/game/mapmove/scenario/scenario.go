5d95e398212fa5ab96194d00be3500d06c78a235
s.miyoshi <s.miyoshi1991@gmail.com>
2023-10-24 21:55:34 +0900

    set map value

--- pkg/app/game/mapmove/scenario/scenario.go
+++ pkg/app/game/mapmove/scenario/scenario.go
@@ -6,19 +6,24 @@ import (
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/mapinfo"
 )
 
+var eno0Args_犬小屋 = event.MapChangeArgs{
+	MapID:   mapinfo.ID_秋原町,
+	InitPos: common.Point{X: 1400, Y: 500},
+}
+
 var Scenario_犬小屋 = [][]event.Scenario{
 	{ // EventNo: 0
-		{Type: event.TypeChangeMapArea},
+		{Type: event.TypeChangeMapArea, Values: eno0Args_犬小屋.Marshal()},
 	},
 }
 
-var eno0Args = event.MapChangeArgs{
+var eno0Args_秋原町 = event.MapChangeArgs{
 	MapID:   mapinfo.ID_犬小屋,
 	InitPos: common.Point{X: 300, Y: 200},
 }
 
 var Scenario_秋原町 = [][]event.Scenario{
 	{ // EventNo: 0
-		{Type: event.TypeChangeMapArea, Values: eno0Args.Marshal()},
+		{Type: event.TypeChangeMapArea, Values: eno0Args_秋原町.Marshal()},
 	},
 }
