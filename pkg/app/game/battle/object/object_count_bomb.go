955fe9c3859d0e5d8e8d8cc71cb3d92fbb30b193
s.miyoshi <s.miyoshi1991@gmail.com>
2023-10-31 12:38:34 +0900

    show bomb image

--- pkg/app/game/battle/object/object_count_bomb.go
+++ pkg/app/game/battle/object/object_count_bomb.go
@@ -1,11 +1,24 @@
 package object
 
 import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/common"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/anim"
+	localanim "github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/anim/local"
+	objanim "github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/anim/object"
+	battlecommon "github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/common"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/damage"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/effect"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/resources"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/sound"
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/dxlib"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/logger"
 )
 
 const (
-	explodeTimeSec = 3
+	explodeTime = 180
 )
 
 type CountBomb struct {
@@ -15,6 +28,16 @@ type CountBomb struct {
 }
 
 func (o *CountBomb) Init(ownerID string, initParam ObjectParam) error {
+	o.pm = initParam
+	o.pm.objectID = uuid.New().String()
+	o.count = 0
+
+	fname := common.ImagePath + "battle/skill/カウントボム.png"
+	o.image = dxlib.LoadGraph(fname)
+	if o.image == -1 {
+		return fmt.Errorf("failed to load image: %s", fname)
+	}
+
 	return nil
 }
 
@@ -23,8 +46,54 @@ func (o *CountBomb) End() {
 }
 
 func (o *CountBomb) Draw() {
+	view := battlecommon.ViewPos(o.pm.Pos)
+	dxlib.DrawRotaGraph(view.X, view.Y+16, 1, 0, o.image, true)
 }
 
 func (o *CountBomb) Process() (bool, error) {
+	o.count++
+
+	if o.pm.HP <= 0 {
+		return true, nil
+	}
+
+	if o.count >= explodeTime {
+		// TODO(ダメージ処理)
+		logger.Info("explode count bomb with %+v", o.pm)
+		return true, nil
+	}
+
+	if o.count%60 == 0 {
+		sound.On(resources.SECountBombCountdown)
+	}
+
 	return false, nil
+}
+
+func (o *CountBomb) DamageProc(dm *damage.Damage) bool {
+	if dm == nil {
+		return false
+	}
+
+	o.pm.HP -= dm.Power
+	localanim.AnimNew(effect.Get(dm.HitEffectType, o.pm.Pos, 5))
+	return true
+}
+
+func (o *CountBomb) GetParam() objanim.Param {
+	return objanim.Param{
+		Param: anim.Param{
+			ObjID:    o.pm.objectID,
+			Pos:      o.pm.Pos,
+			DrawType: anim.DrawTypeObject,
+		},
+		HP: o.pm.HP,
+	}
+}
+
+func (o *CountBomb) GetObjectType() int {
+	return objanim.ObjTypeNone
+}
+
+func (o *CountBomb) MakeInvisible(count int) {
 }
--- pkg/app/game/battle/skill/skill_count_bomb.go
+++ pkg/app/game/battle/skill/skill_count_bomb.go
@@ -1,9 +1,15 @@
 package skill
 
 import (
+	"fmt"
+
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/common"
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/draw"
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/anim"
+	localanim "github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/anim/local"
+	objanim "github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/anim/object"
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/field"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/battle/object"
 )
 
 type countBomb struct {
@@ -25,9 +31,28 @@ func (p *countBomb) Draw() {
 }
 
 func (p *countBomb) Process() (bool, error) {
+	const endCount = 60
+
 	if p.count == 0 {
-		field.SetBlackoutCount(150)
+		field.SetBlackoutCount(endCount)
 		SetChipNameDraw("カウントボム", true)
+	}
+
+	if p.count == endCount {
+		pos := localanim.ObjAnimGetObjPos(p.Arg.OwnerID)
+		obj := &object.CountBomb{}
+		pm := object.ObjectParam{
+			Pos:           common.Point{X: pos.X + 1, Y: pos.Y},
+			HP:            50,
+			OnwerCharType: objanim.ObjTypePlayer,
+			Power:         int(p.Arg.Power),
+		}
+		if err := obj.Init(p.ID, pm); err != nil {
+			return false, fmt.Errorf("count bomb create failed: %w", err)
+		}
+		atkID := localanim.ObjAnimNew(obj)
+		localanim.ObjAnimAddActiveAnim(atkID)
+		return true, nil
 	}
 
 	p.count++
