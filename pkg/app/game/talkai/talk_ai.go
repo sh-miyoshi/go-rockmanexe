b446e901f7e8ab91345844f9e9c6fcc9bafeff6a
s.miyoshi <s.miyoshi1991@gmail.com>
2023-12-03 23:08:01 +0900

    show background

--- pkg/app/game/talkai/talk_ai.go
+++ pkg/app/game/talkai/talk_ai.go
@@ -1,6 +1,9 @@
 package talkai
 
-import "github.com/sh-miyoshi/go-rockmanexe/pkg/app/window"
+import (
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/game/background"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/window"
+)
 
 const (
 	stateInput = iota
@@ -15,17 +18,23 @@ var (
 func Init() {
 	state = stateInput
 	win.Init()
+	background.Set(background.Type秋原町)
+	win.SetMessage("", window.FaceTypeRockman)
 }
 
 func End() {
 	win.End()
+	background.Unset()
 }
 
 func Draw() {
+	background.Draw()
 	win.Draw()
 }
 
 func Process() bool {
+	background.Process()
+
 	switch state {
 	case stateInput:
 		// TODO
