ad5829101a2c0d5aeaaf741f074db9793649fab5
s.miyoshi <s.miyoshi1991@gmail.com>
2023-12-22 20:50:35 +0900

    update debug info view

--- pkg/app/system/system.go
+++ pkg/app/system/system.go
@@ -2,12 +2,17 @@ package system
 
 import (
 	"errors"
+	"fmt"
 
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/app/config"
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/dxlib"
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/logger"
 )
 
 var (
 	unrecoverableError error
+	debugMessages      []string
+	font               int = -1
 )
 
 func SetError(msg string) {
@@ -20,4 +25,26 @@ func SetError(msg string) {
 
 func Error() error {
 	return unrecoverableError
+}
+
+func AddDebugMessage(format string, a ...interface{}) {
+	debugMessages = append(debugMessages, fmt.Sprintf(format, a...))
+}
+
+func DebugDraw() {
+	if config.Get().Debug.ShowDebugData {
+		if font == -1 {
+			font = dxlib.CreateFontToHandle(dxlib.CreateFontToHandleOption{
+				FontName: nil,
+				Size:     dxlib.Int32Ptr(22),
+				Thick:    dxlib.Int32Ptr(7),
+				FontType: dxlib.Int32Ptr(dxlib.DX_FONTTYPE_EDGE),
+			})
+		}
+
+		for i, msg := range debugMessages {
+			dxlib.DrawFormatStringToHandle(0, i*25, 0xffffff, font, msg)
+		}
+		debugMessages = []string{}
+	}
 }
