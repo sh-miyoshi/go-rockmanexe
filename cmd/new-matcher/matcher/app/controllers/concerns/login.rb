b18b2f67535b36f62e30a2db1269fcc13c408a5b
s.miyoshi <s.miyoshi1991@gmail.com>
2024-02-25 08:56:28 +0900

    add api concerns

---
+++ cmd/new-matcher/matcher/app/controllers/concerns/login.rb
@@ -0,0 +1,17 @@
+module Login
+  extend ActiveSupport::Concern
+
+  private
+
+  def login_user
+    # redirect top page if not logged in
+    return redirect_to "/" unless session[:user_id].present?
+
+    user = User.find_by(login_id: session[:user_id])
+
+    # redirect user create page if not in matching server
+    return redirect_to user_new_path if user.nil?
+
+    user
+  end
+end
