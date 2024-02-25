bfe20c1d7286f8c1b6df68c9424382240a94cdb8
s.miyoshi <s.miyoshi1991@gmail.com>
2024-02-25 14:55:51 +0900

    add new page

--- cmd/new-matcher/matcher/app/controllers/users_controller.rb
+++ cmd/new-matcher/matcher/app/controllers/users_controller.rb
@@ -8,11 +8,32 @@ class UsersController < ApplicationController
   end
 
   def new
+    redirect_to "/" unless session[:user_id].present?
   end
 
   def create
+    return redirect_to "/" unless session[:user_id].present?
+
+    user_id = SecureRandom.uuid
+    User.create!(
+      user_id: user_id,
+      name: params[:name],
+      login_id: session[:user_id]
+    )
+
+    redirect_to user_path(user_id)
+  rescue StandardError => e
+    Rails.logger.info("Failed to create user: #{e}")
+    flash[:danger] = "ユーザーの作成に失敗しました。<br/>#{e}"
+    redirect_to new_user_path
   end
 
   def destroy
+  end
+
+  private
+
+  def create_params
+    params.permit(:name)
   end
 end
