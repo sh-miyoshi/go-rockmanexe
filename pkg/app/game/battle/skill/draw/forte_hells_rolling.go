a3d50fff3ee28d502d4b8ed499653c3087368eb1
s.miyoshi <s.miyoshi1991@gmail.com>
2024-09-01 08:29:59 +0900

    show image

--- pkg/app/game/battle/skill/draw/forte_hells_rolling.go
+++ pkg/app/game/battle/skill/draw/forte_hells_rolling.go
@@ -1,6 +1,7 @@
 package skilldraw
 
 import (
+	"github.com/sh-miyoshi/go-rockmanexe/pkg/dxlib"
 	"github.com/sh-miyoshi/go-rockmanexe/pkg/utils/point"
 )
 
@@ -8,4 +9,6 @@ type DrawForteHellsRolling struct {
 }
 
 func (p *DrawForteHellsRolling) Draw(viewPos point.Point, count int) {
+	n := count / 10 % len(imgForteHellsRolling)
+	dxlib.DrawRotaGraph(viewPos.X, viewPos.Y, 1.0, 0.0, imgForteHellsRolling[n], true)
 }
